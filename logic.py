c486690a1852e85bf34b7722e403e41ac90f56e2
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-08-15 16:50:04 -0700

    sendblue works

--- logic.py
+++ logic.py
@@ -2,10 +2,13 @@ import threading
 import uuid
 
 from flask import Flask, Response, request
-from twilio.rest import Client
-from twilio.twiml.messaging_response import MessagingResponse
+from sendblue import Sendblue
 
-from keys import twilio_key
+from keys import twilio_key, sendblue_key, sendblue_secret
+
+
+sendblue = Sendblue(sendblue_key, sendblue_secret)
+
 
 USER_ID_TO_TYPE_DICT = {1: "pizza", 2: "barbie", 3: "oppenheimer"}
 USER_ID_TO_CONTEXT = {
@@ -13,10 +16,6 @@ USER_ID_TO_CONTEXT = {
     2: f"Ken wants to get to know Barbie better and convince her to go on a date.",
     3: "It's 1945. Another person wants to convince Einstein to not blow up the world. You don't know their name, but you want to blow up the world because you're research paper didn't get any attention.",
 }
-
-account_sid = "ACfe453c3ef8654f6859d7aab0e23cd10f"
-twilio_number = "+14066312474"
-client = Client(account_sid, twilio_key)
 
 def talk(user, new_message):
     """Talk with a specific player."""
@@ -28,8 +27,6 @@ def talk(user, new_message):
     print("NEW MESSAGE")
     print(new_message)
 
-    client.messages.create(
-        body="Hi",
-        from_=twilio_number,
-        to=user['number'],
-    )
+    sendblue.send_message('+12812240743', {
+        'content': 'Hello from Sendblue!',
+    })
