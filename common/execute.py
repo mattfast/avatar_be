55c007591bc0e7d7519239a8825f3aaae7e71bfe
Akash Samant <asamant@princeton.edu>
2023-09-08 00:04:35 -0700

    change

--- common/execute.py
+++ common/execute.py
@@ -1,7 +1,7 @@
 from langchain.schema import SystemMessage
 
 from common.prompt import Prompt
-from constants import MODEL_DICT, chat_models_list
+from constants import MODEL_DICT, chat_models_set
 
 
 def clean_response(result: str) -> str:
@@ -15,7 +15,7 @@ def compile_and_run_prompt(prompt_cls, dict_options: dict, **kwargs):
         final_prompt = prompt_cls(dict_options)
     except:
         raise ValueError("Missing dictionary Keys")
-    if final_prompt.model in chat_models_list:
+    if final_prompt.model in chat_models_set:
         return run_chat_prompt(
             final_prompt.template,
             verbose=final_prompt.verbose,
