85ce2d7cb69d87eaa594c45a3dcf7ed862b01af0
Matthew Fastow <55714940+mattfast@users.noreply.github.com>
2023-09-09 17:08:15 -0700

    add more cors allowed origins

--- application.py
+++ application.py
@@ -28,7 +28,7 @@ CORS(app)
 app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(
     app,
-    cors_allowed_origins=["http://localhost:3000"],
+    cors_allowed_origins=["http://localhost:3000", "https://milk-ai.com", "https://milk-ai.com/chat"],
     async_mode="threading",
     transports=["websocket"],
 )
