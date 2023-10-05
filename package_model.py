717a3b78c222caf8490a9a1abeab2af66ccacc15
Akash Samant <asamant@princeton.edu>
2023-10-05 01:10:59 -0700

    change

--- package_model.py
+++ package_model.py
@@ -8,8 +8,12 @@ from pathlib import Path
 import boto3
 import requests
 import sagemaker
+import logging
 
 from dbs.mongo import mongo_read, mongo_upsert
+
+logging.basicConfig(level=logging.INFO)
+
 
 # Count number of directories in path
 
@@ -38,14 +42,14 @@ def package_model(user_id):
         number_of_folders = 0
 
     if number_of_folders >= 10:
-        print(f"Number of folders {number_of_folders}")
+        logging.info(f"Number of folders {number_of_folders}")
         return
 
     training_job = mongo_read("UserTrainingJobs", {"user_id": user_id})
     url = training_job.get("job_url", None)
 
     if url is None:
-        print(f"URL IS NONE")
+        logging.info(f"URL IS NONE")
         return
 
     # Upsert Directory to Say Model Has been uploaded
@@ -56,54 +60,54 @@ def package_model(user_id):
     )
 
     try:
-        print("ENTERING LOOP")
+        logging.info("ENTERING LOOP")
         res2 = requests.get(url, headers=headers)
         loaded_resp = json.loads(res2.text)
         model_url = loaded_resp["dreambooth_result"]["checkpoints"][0]["url"]
-        print(f"Got Model URL {model_url}")
+        logging.info(f"Got Model URL {model_url}")
 
         user_name = user_id
         base_path = Path(model_folder_name) / user_name
         artifacts_folder = Path(model_folder_name) / "artifacts"
 
-        print(f"Creating Base Path {str(base_path)}")
+        logging.info(f"Creating Base Path {str(base_path)}")
         base_path.mkdir(parents=True, exist_ok=True)
 
         model_version_path = base_path / "1"
-        print(f"Creating Model Version Path {str(model_version_path)}")
+        logging.info(f"Creating Model Version Path {str(model_version_path)}")
         model_version_path.mkdir(parents=True, exist_ok=True)
 
-        print("Writing config.pbtxt file")
+        logging.info("Writing config.pbtxt file")
         # Write config.pbtxt
         to_write = open(base_path / "config.pbtxt", "w+")
         to_write.write(f'name: "{user_name}"\n')
         to_write.write(open(artifacts_folder / "config.pbtxt", "r").read())
         to_write.close()
 
-        print("Writing model.py file")
+        logging.info("Writing model.py file")
         # Write Model.PY
         py_file_write = open(model_version_path / "model.py", "w+")
         py_file_write.write(open(artifacts_folder / "model.py", "r").read())
         py_file_write.close()
 
-        print("Loading Safetensors file")
+        logging.info("Loading Safetensors file")
         # Write safetensors file
         response = requests.get(model_url, stream=True)
         with open(model_version_path / "custom_model.safetensors", mode="wb") as file:
             for chunk in response.iter_content(chunk_size=10 * 1024):
                 file.write(chunk)
 
-        print("Writing tar file")
+        logging.info("Writing tar file")
         save_file_name = f"packaged_models/{user_id}.tar.gz"
         with tarfile.open(save_file_name, "w:gz") as tar:
             tar.add(base_path, arcname=os.path.basename(base_path))
 
-        print("Uploading file to S3")
+        logging.info("Uploading file to S3")
         sagemaker_session.upload_data(
             path=save_file_name, bucket=bucket, key_prefix=prefix
         )
 
-        print("Removing Directories")
+        logging.info("Removing Directories")
         shutil.rmtree(base_path, ignore_errors=True)
         os.remove(save_file_name)
 
@@ -115,8 +119,8 @@ def package_model(user_id):
         )
     except Exception as e:
         # Upsert Directory to Say Model Has been uploaded
-        print("JUST EXCEPTED")
-        print(e)
+        logging.info("JUST EXCEPTED")
+        logging.info(e)
         mongo_upsert(
             "UserTrainingJobs",
             {"user_id": user_id},
