5ac2c136164b15d29c2fae92d5bd223af094ce39
Akash Samant <asamant@princeton.edu>
2023-10-10 18:03:24 -0700

    change

--- run_inference.py
+++ run_inference.py
@@ -304,7 +304,7 @@ def generate_all_images(user_id):
         num_funny_styles = 0
         has_animal_style = False
         # Generate 10 images for the user
-        for i in range(10):
+        for i in range(20):
             key = f"{user_id}/profile_{i}.png"
             logging.info("CHOOSING RANDOM CATEGORY FROM LIST")
             category = random.choice(actual_choice_list)
