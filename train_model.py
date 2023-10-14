3176d7317016293033acb436f7b72e05daf0fb61
Akash Samant <asamant@princeton.edu>
2023-10-14 13:57:30 -0700

    change

--- train_model.py
+++ train_model.py
@@ -140,7 +140,7 @@ def check_job_until_finished(job_url, user_id):
 
 def _exec_subprocess(cmd: str):
     """Executes subprocess and prints log to terminal while subprocess is running."""
-    logging.info(subprocess.run("/home/ubuntu/.local/bin/modal --help", shell=True, env={'PYTHONPATH': '/home/ubuntu/.local/lib/python3.10/site-packages/modal'}))
+    logging.info(subprocess.run("modal --help", shell=True, env={'PYTHONPATH': '/home/ubuntu/.local/lib/python3.10/site-packages/'}))
     # process = subprocess.Popen(
     #     [cmd],
     #     stdout=subprocess.PIPE,
